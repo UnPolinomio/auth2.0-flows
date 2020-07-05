be89df70e8934bc047bdc3e032f47dfcbea12e80
Hector Franco T <tor6.franco6@hotmail.com>
2020-07-05 18:05:18 -0500

    Verify token

--- api/index.js
+++ api/index.js
@@ -12,9 +12,26 @@ app.use(bodyParser.json())
 app.post('/api/auth/token', ( req, res ) => {
     const { email, username, name } = req.body
 
-    const token = jwt.sign({ sub: username, email, name }, config.authJwtSecret)
+    const token = jwt.sign({ sub: username, email, name }, config.authJwtSecret, {
+        expiresIn: 700
+    })
 
     res.json({ access_token: token })
+})
+
+app.post('/api/auth/verify', ( req, res, next ) => {
+    const { access_token } = req.query
+
+    try {
+        const decoded = jwt.verify(access_token, config.authJwtSecret)
+
+        res.json({
+            message: "Valid access token.",
+            username: decoded.sub
+        })
+    } catch (err) {
+        next(err)
+    }
 })
 
 let server = app.listen(5000, () => {
